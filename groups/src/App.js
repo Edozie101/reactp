7cc662c60d4ac310b2140530ee0c136155c5cdf7
edzye101 <edzye101@gmail.com>
2018-05-09 21:37:18 +0200

    just added the react app again

---
+++ groups/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import ChordEditor from './components/ChordEditor';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>
+        Hello world
+      </h1>
+    );
+  }
+}
+
+export default App;
