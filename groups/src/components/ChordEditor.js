83919240320ce758111b4185c914bb62a1a0da9b
edzye101 <edzye101@gmail.com>
2018-05-19 19:14:33 +0200

    adjusted changes

---
+++ groups/src/components/ChordEditor.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ChordEditor extends Component {
+
+
+
+  render(){
+    return (
+      <h1> Chords</h1>
+    )
+  }
+}
+
+export default ChordEditor
---
+++ groups/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+
+  render(){
+    return(
+      <header>
+      <h1> Chord Editor</h1>
+      </header>
+
+    )
+
+  }
+
+}
+
+export default Header;
